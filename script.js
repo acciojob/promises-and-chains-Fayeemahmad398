650c8124c1230b645bf5304b31ff72f6e92586ad
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-03-29 21:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+
+var ageElement=document.querySelector("#age");
+var nameElement=document.querySelector("#name");
+var btnElement=document.querySelector("btn");
+
+var promise=new Promise(function (resolve,reject) {
+setTimeOut(function () {
+	if(!(age & name)){
+		return reject("Error");
+	}
+	if(age>18){
+		return resolve(`Welcome, ${name} You can vote.`)
+	}else{
+		return reject(`Oh sorry ${age} You aren't old enough.`)
+	}
+},4000)
+	
+})
+	
+
+
+
+
+function callPromise() {
+	return promise;
+}
+
+
+
+function HandleEventPromise() {
+	callPromise.then(alert("DONE")).catch(alert("Error"));
+	
+}
+
+
+btnElement.addEventListener("click",HandleEventPromise());
