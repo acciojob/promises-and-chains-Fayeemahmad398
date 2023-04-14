024939ddd4e843c624dc98a6473dfa96c0d25ffe
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-04-14 16:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ setTimeOut(function () {
 	if(age>18){
 		return resolve(`Welcome, ${name} You can vote.`)
 	}else{
-		return reject(`Oh sorry ${age} You aren't old enough.`)
+		return reject(`Oh sorry ${name} You aren't old enough.`)
 	}
 },4000)
 	
